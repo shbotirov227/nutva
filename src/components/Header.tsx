4b11d8754cf308910c8c3e122ddf51925500a946
abikIbragim <abikibragim@icloud.com>
2025-06-19 19:57:21 +0500

    no responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,13 +47,12 @@ const Header: React.FC = () => {
 
     return (
         <header
-            className="max-w-[375px]:border-red-600 sm:w-full sm:h-[80px] sm:border-red-500 fixed top-0 w-full box-border text-white py-7 shadow-md backdrop-blur-xl z-50 transition-transform duration-300"
+            className="fixed top-0 w-full box-border text-white py-7 shadow-md backdrop-blur-xl z-50 transition-transform duration-300"
             style={{
                 backgroundColor: `rgba(20, 20, 20, 0.7)`,
                 boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
                 backdropFilter: "blur(10px)",
                 transform: visible ? "translateY(0)" : "translateY(-100%)",
-
             }}
         >
             <Container>
@@ -62,7 +61,7 @@ const Header: React.FC = () => {
                         <Image src="/nutva-logo.png" alt="Logo" width={150} height={150} className="inline-block mr-2" />
                     </Link>
                     <nav className="mt-2">
-                        <ul className=" flex space-x-5">
+                        <ul className="flex space-x-5">
                             <li><NavLink href="/">Главная</NavLink></li>
                             <li><NavLink href="/product">Продукты</NavLink></li>
                             <li><NavLink href="/about-us">О бренде</NavLink></li>
