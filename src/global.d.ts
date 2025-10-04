bf7c0ea8d8eaef7def51993724b7a2064079f5b4
shbotirov227 <shbotirov227@gmail.com>
2025-10-04 17:59:30 +0500

    fixed config

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,10 +4,10 @@ declare module "*.sass";
 
 
 // Global declaration for GA helper injected in layout head
-declare global {
+// declare global {
 	interface Window {
 		gtagSendEvent?: (url: string) => boolean;
 	}
-}
+// }
 
 // export {};
